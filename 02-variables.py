333210207c81baf6f7e2d52c44bf0f70fc5f32a2
Nico Arato <42045046+nicoarato@users.noreply.github.com>
2019-11-22 13:25:54 -0300

    Convención de variables para python.

---
+++ 02-variables.py
@@ -0,0 +1,14 @@
+#No valido : comenzar con un número.
+
+hola = "Hola mundo!!"
+nombreCliente = "Pepe" #camel case
+NombreCliente = "Pepe" #Pascal case o capital camel case
+nombrecliente = "Pepe" #flat case
+#nombre-cliente = "Pepe" #kebab case o dash case
+nombre_cliente = "Pepe" #snake case   :)
+
+#Cuidado! case sensitive
+
+
+print(nombre_cliente)
+
