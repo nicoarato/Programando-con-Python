b593269755e552c0ae97ef555fe1e730e612c352
Nico Arato <42045046+nicoarato@users.noreply.github.com>
2019-11-25 11:01:25 -0300

    Numeros y un detalle.

---
+++ 05-numero.py
@@ -0,0 +1,28 @@
+print(2)
+print(2.5)
+print(2 + 3)
+
+
+#Operadores
+
+print(10/3)
+print(10*3)
+print(10-3)
+
+
+print(10**3) #potencia
+print(3**3)
+
+print((2*3) + 4 )
+
+numero=20
+print(numero)
+numero+=5
+print(numero)
+
+
+#numero++ NO EXISTE
+
+numero+=1
+print(numero)
+
