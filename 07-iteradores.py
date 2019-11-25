be8a08ab8774e886573b2ee65379d1139739d635
Nico Arato <42045046+nicoarato@users.noreply.github.com>
2019-11-25 12:09:11 -0300

    Iteradores.

---
+++ 07-iteradores.py
@@ -0,0 +1,15 @@
+lenguajes = ['javascript','ruby', 'python', 'c++', 'php', 'java']
+
+#iterador
+for lenguaje in lenguajes:
+    print(lenguaje)
+
+
+#con numeros
+for num in range(0,10,2):
+    print(num)
+
+#con indices numericos
+cant = len(lenguajes)
+for i in range(0,cant):
+    print(lenguajes[i])
